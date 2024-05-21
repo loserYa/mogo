a2dedb0b81f5c4fb9c7d9da8992c6add35dda9ef
loser <1500162516@qq.com>
2024-05-21 21:36:43 +0800

    修正依赖

--- mogo-spring-boot-starter/src/main/java/io/github/loser/config/MogoAutoConfiguration.java
+++ mogo-spring-boot-starter/src/main/java/io/github/loser/config/MogoAutoConfiguration.java
@@ -96,9 +96,9 @@ public class MogoAutoConfiguration {
                         " | |  | | | |__| | | |__| | | |__| |\n" +
                         " |_|  |_|  \\____/   \\_____|  \\____/"
         );
-        System.out.println(":: Mogo starting ::           v1.1.0");
+        System.out.println(":: Mogo starting ::           v1.1.2");
         System.out.println(":: gitee         ::           https://gitee.com/lyilan8080/mogo");
-        System.out.println(":: doc           ::           http://mogo.loser.plus");
+        System.out.println(":: doc           ::           https://loser.plus");
         System.out.println(":: author        ::           loser");
         System.out.println();
         if (MogoEnableCache.base) {
